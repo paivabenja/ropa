cb196259230d2eed04250c289120062820f03fb3
paivabenja <paivabenja05@gmail.com>
2023-04-12 08:07:55 -0300

    implementing image server on frontend

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -4,11 +4,15 @@ import productos from './prods'
 
 
 export default function Home() {
-  const [clothesStock, setClothesStock] = useState()
+  const [clothesStock, setClothesStock] = useState([])
 
   const getClothesStock = () => {
-    fetch('http://localhost:8000/api/ropa/', { mode: 'no-cors' })
+    fetch('http://localhost:8000/api/ropa/', { mode: 'cors' })
       .then(r => r.json())
+      .then(r => {
+        console.log('response: ' + r)
+        return r
+      })
       .then(r => setClothesStock(r))
       .catch(console.log('error amigo'))
   }
@@ -21,17 +25,18 @@ export default function Home() {
 
   return (
     <div className="flex flex-wrap mt-4 m-12 justify-center items-start text-white content-start gap-12 z-10">
-      {[0, 1, 2, 3, 4,5,6,7,8,9].map((i) => {
+      {clothesStock.map((i) => {
+        console.log('console.log de i: ', i)
         return (
           <Item
             key={i}
-          type={productos[0].type}
-          title={productos[0].title}
-          sizes={productos[0].sizes}
-          imgFront={productos[0].imgFront}
-          imgBack={productos[0].imgBack}
-          price={productos[0].price}
-          stock={productos[0].stock}
+            type={clothesStock[0].type}
+            title={clothesStock[0].title}
+            sizes={clothesStock[0].sizes}
+            imgFront={clothesStock[0].imgFront}
+            imgBack={clothesStock[0].imgBack}
+            price={clothesStock[0].price}
+            stock={clothesStock[0].stock}
           ></Item>
         );
       })}
